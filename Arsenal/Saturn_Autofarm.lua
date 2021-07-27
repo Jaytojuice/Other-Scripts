74d92367c2454d428afa7535a18cb4a3d6e10857
Jaytojuice <85552882+Jaytojuice@users.noreply.github.com>
2021-07-27 18:41:44 +0800

    Create Saturn_Autofarm.lua

---
+++ Arsenal/Saturn_Autofarm.lua
@@ -0,0 +1,5 @@
+_G.Key = "SaturnOnTop" -- Key Here
+_G.ChatSpam = true
+_G.SpamMessage = "Saturn On Top"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Saturn-Scripts/Saturn/main/FixedArsenalAuto", true))()
