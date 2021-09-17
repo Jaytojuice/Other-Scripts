e25a7b781a38ca4979d6ba9061c1da43a06ad75e
Jaytojuice <85552882+Jaytojuice@users.noreply.github.com>
2021-09-17 10:22:09 +0800

    Delete Arsenal directory

--- Arsenal/Anti_Crash.lua
+++
@@ -1,7 +0,0 @@
-local old=require(game.ReplicatedStorage.Modules.ClientFunctions).CreateProjectile
-require(game.ReplicatedStorage.Modules.ClientFunctions).CreateProjectile=function(a,...)
-    if tostring(a)~=game.Players.LocalPlayer.Name then
-        return
-    end
-    return old(a,...)
-end
--- Arsenal/Saturn_Autofarm.lua
+++
@@ -1,5 +0,0 @@
-_G.Key = "SaturnOnTop" -- Key Here
-_G.ChatSpam = true
-_G.SpamMessage = "Saturn On Top"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Saturn-Scripts/Saturn/main/FixedArsenalAuto", true))()
