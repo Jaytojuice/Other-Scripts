489b517e2b50b861320604bdb852038cca64b9b8
Jaytojuice <85552882+Jaytojuice@users.noreply.github.com>
2021-09-17 10:50:33 +0800

    Create Unban.lua

---
+++ Arsenal/Unban.lua
@@ -0,0 +1,5 @@
+scriptname="Notification";loadstring(game:HttpGetAsync("https://guard.lol/scripts/guardnotify.lua"))("https://guard.lol")
+
+guardNotify("Notification","Teleporting To Testing Server", 5, Color3.fromRGB(49, 55, 66), Color3.fromRGB(255, 102, 88), Color3.fromRGB(178, 255, 190), Color3.fromRGB(0, 0, 0), "rbxassetid://57254792", "Sine")
+
+game:GetService("TeleportService"):Teleport(7057165181, LocalPlayer)
